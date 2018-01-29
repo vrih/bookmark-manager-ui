55beb944f72240d8ab63176a1cdf6e38bd8bad26
Daniel Bowman <daniel@danielbowman.co.uk>
2018-01-29 13:42:12 +0000

    Fixes #1 (#2)

--- profiles.clj
+++ profiles.clj
@@ -13,7 +13,7 @@
 {:shared {:clean-targets ["out" :target-path]
           :test-paths ["test/clj" "test/cljs"]
           :resources-paths ["dev-resources"]
-          :plugins [[com.cemerick/clojurescript.test "0.3.1"]]
+;          :plugins [[com.cemerick/clojurescript.test "0.3.1"]]
           :cljsbuild
           {:builds {:bookmark-html
                     {:source-paths ["test/cljs"]
@@ -23,23 +23,15 @@
            :test-commands {"phantomjs"
                            ["phantomjs" :runner "dev-resources/public/js/bookmark_html.js"]}}}
  :dev [:shared
-       {:source-paths ["dev-resources/tools/http" "dev-resources/tools/repl"]
-        :dependencies [[ring "1.3.1"]
-                       [compojure "1.2.1"]
-                       [enlive "1.1.5"]]
-        :plugins [[com.cemerick/austin "0.1.5"]]
+       {
         :cljsbuild
         {:builds {:bookmark-html
-                  {:source-paths ["dev-resources/tools/repl"]
+                  {
                    :compiler
                    {:optimizations :whitespace
                     :pretty-print true}}}}
         
-        :injections [(require '[ring.server :as http :refer [run]]
-                              'cemerick.austin.repls)
-                     (defn browser-repl []
-                       (cemerick.austin.repls/cljs-repl (reset! cemerick.austin.repls/browser-repl-env
-                                                                (cemerick.austin/repl-env))))]}]
+        }]
  ;; simple profile.
  :simple [:shared
           {:cljsbuild
